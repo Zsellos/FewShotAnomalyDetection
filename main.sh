a8299320413b897ae0fe304fe3a8d7e9fa1dbb28
zoe.sellos <zoe.sellos@gmail.com>
2023-11-08 19:47:44 +0100

    Added some training condition

--- main.sh
+++ main.sh
@@ -3,33 +3,47 @@
 echo "Different action will be asked to help you train a simple dataset."
 # echo "Only write the needed parameters"
 
-echo "First, on witch dataset would you like to train the models ? Available datasets are cifar/mnist/fashionmnist/mvtec/paris." # dataset
-read dataset
-echo "Chosen dataset : " $dataset
+main()
+{
+    f_dataset()
+    {
+        echo "On witch dataset would you like to train the models ? Available datasets are cifar/mnist/fashionmnist/mvtec/paris." # dataset
+        read dataset
 
-echo "Do you want default values to train this dataset ? [yes/no] (If yes, no more question will be asked and the training of the selected dataset will start)"
-read train_instant
+        if [ -z "$dataset" ]
+        then
+            echo empty
+            dataset=cifar
+        fi
+        echo "Choosen dataset : " $dataset
 
-if [[ $train_instant == *"yes"* ]]
-then
-    python main_train.py  --num_images 1  --pos_class plane --index_download 0 --dataset $dataset
-else
-    echo No
-fi
+            echo "Confirm dataset ? [yes/no]"
+        read data_conf
 
-read -p "Enter your name: " name
-echo Hello $name!
-
-# python main_train.py  --num_images <num_training_images>  --pos_class <normal_class_in_dataset> --index_download <index_of_training_image> --dataset $dataset
+        if [[ $data_conf != *"y"* ]]
+        then
+            f_dataset
+        fi
+    }
+    f_dataset
 
 
-# --min_size                  image minimal size at the coarser scale (default 25)
-# --max_size                  image minimal size at the coarser scale (default 64)
-# --niter                     number of iterations to train per scale
-# --num_images                number of images to train on (1,5,10 in the paper)
-# --size_image                the original image size 
-# --fraction_defect           the number of patches to consider in defect detection (recommended arguments: 0.01-0.1)
-# --pos_class                 the normal class to train on
-# --dataset                   paris/cifar/mnist/fashionmnist/mvtec
-# --random_images_download    "True" if training random images from the normal class (otherwise, specify the index of the training image in --index_download)
-# --devices_ids                for 10shot we have used --device_ids = 0 1     
+
+
+
+    # FIXME : Need to add elements of confirmation for each elements
+
+    echo "Do you want default values to train this dataset ? [yes/no] (If yes, no more question will be asked and the training of the selected dataset will start)"
+    read train_instant
+
+    if [[ $train_instant == *"y"* ]]
+    then
+        python main_train.py  --num_images 1  --pos_class plane --index_download 0 --dataset $dataset
+    else
+        echo No
+    fi
+
+}
+
+main
+
